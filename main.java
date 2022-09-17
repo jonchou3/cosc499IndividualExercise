042e0b07816542656bd7b5421014554226e3f3ea
Jonathan Chou <jonchou3@gmail.com>
2022-09-16 21:41:08 -0700

    commit to main

---
+++ main.java
@@ -0,0 +1,19 @@
+public class main {
+    void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    // swap arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+    }
+    public static void main(String args[]){
+        main ob = new main();
+        int arr[] = { 64, 34, 25, 12, 22, 11, 90 };
+        ob.bubbleSort(arr);
+    }
+}
