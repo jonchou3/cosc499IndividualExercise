974c63fa74918aa5ed4d8012cd11883831e2deb5
Jonathan Chou <jonchou3@gmail.com>
2022-09-16 22:36:54 -0700

    created new branch

--- exercise.java
+++ exercise.java
@@ -5,16 +5,16 @@
  * @version Sep/16/2022
  */
 public class exercise{
-    int addition(int a, int b)
+    int multiplication(int a, int b)
     {
-        int sum = a + b;
-        return sum;    
+        int product = a * b;
+        return product;    
     }
 
     public static void main(String args[]){
         exercise ob = new exercise();
         int x = 35, y = 25;
-        int result = ob.addition(x,y);
+        int result = ob.multiplication(x,y);
         System.out.println(result);
     }
 }
